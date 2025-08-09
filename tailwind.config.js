55def22688764ba7c0ea6339326b680fe518d668
KUPSCHUS ABIGAELLE <abigaelle.kupschus@etu.unistra.fr>
2025-08-09 18:18:28 +0200

    Back-end réalisé et css tailwind installé

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{twig,html,js,html.twig}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
