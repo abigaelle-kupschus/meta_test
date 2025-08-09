e51bbe51b14dceb12053a9ecf24bc9a0a99b606a
KUPSCHUS ABIGAELLE <abigaelle.kupschus@etu.unistra.fr>
2025-08-09 20:32:09 +0200

    Css fait

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -50,7 +50,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('_profiler_home');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -77,6 +77,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
